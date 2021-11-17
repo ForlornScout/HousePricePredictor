4503717260c0f2e4aeba7e70da270acee191bb7c
Nate Arkin <nate.pcwiz@me.com>
2021-11-16 18:53:06 -0600

    Updates to support landing page at /

--- apiServer.py
+++ apiServer.py
@@ -1,7 +1,7 @@
 # REST API for AI Project - CS464
 # Written by Nate Arkin and Max Herrick
 
-from flask import Flask
+from flask import Flask, send_file, abort
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
@@ -16,6 +16,15 @@ class Test(Resource):
         return {'data': data}, 200
 
 api.add_resource(Test, '/test')
+
+class MainPage(Resource):
+    def get(self):
+        try:
+            return send_file('Web/index.html')
+        except FileNotFoundError:
+            abort(404)
+
+api.add_resource(MainPage, '/')
 
 if __name__ == '__main__':
     app.run()
