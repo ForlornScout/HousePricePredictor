ab888c69741c49544065f4e5dbb7f9594092a9f8
Nate Arkin <nate.pcwiz@me.com>
2021-12-06 21:38:16 -0600

    Added getHousePrice API

--- apiServer.py
+++ apiServer.py
@@ -4,6 +4,7 @@
 from flask import Flask, send_file, abort, request
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
+from sklearn import tree
 import ast
 
 app = Flask(__name__)
@@ -88,6 +89,46 @@ class getSimilarHouses(Resource):
         return data.to_json(orient="records")
 
 api.add_resource(getSimilarHouses, '/api/getSimilarHouses')
+
+class getHousePrice(Resource):
+    def post(self):
+        featuresRequired = {}
+        
+        for key in request.form:
+            if key == 'noBedrooms':
+                featuresRequired["BedroomAbvGr"] = int(request.form['noBedrooms'])
+            elif key == 'noFullBaths':
+                featuresRequired["FullBath"] = int(request.form['noFullBaths'])
+            elif key == 'noHalfBaths':
+                featuresRequired["HalfBath"] = int(request.form['noHalfBaths'])
+            elif key == 'noCarGarage':
+                featuresRequired["GarageCars"] = int(request.form['noCarGarage'])
+            elif key == 'overallQuality':
+                featuresRequired["OverallQual"] = int(request.form['overallQuality'])
+            elif key == 'overallCond':
+                featuresRequired["OverallCond"] = int(request.form['overallCond'])
+
+        data = pd.read_csv('Dataset/train.csv')
+        x = data[['BedroomAbvGr', 'FullBath', 'HalfBath', 'GarageCars', 'OverallQual', 'OverallCond']]
+        y = data[['SalePrice']]
+        classifier = tree.DecisionTreeClassifier()
+        classifier.fit(x, y)
+
+        predictionData = {}
+        for key in ['BedroomAbvGr', 'FullBath', 'HalfBath', 'GarageCars', 'OverallQual', 'OverallCond']:
+            found = False
+            for feature in featuresRequired:
+                if key == feature:
+                    predictionData[feature] = featuresRequired[key]
+                    found = True
+            if not found:
+                predictionData[key] = data[key].mean()
+        prediction = pd.DataFrame(predictionData, index=[0])
+        predictedPrice = classifier.predict(prediction)
+        print(predictionData)
+        return int(predictedPrice[0])
+
+api.add_resource(getHousePrice, '/api/getHousePrice')
 
 if __name__ == '__main__':
     app.run(port=5002)
